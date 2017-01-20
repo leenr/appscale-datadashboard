d41e15422d9566201c79dad7090c8d1d2a651007
leenr <i@leenr.ru>
2017-01-20 19:36:34 +0300

    Fix columns

--- settings/local.py
+++ settings/local.py
@@ -2,6 +2,9 @@
 #UASERVER_HOST = '172.17.0.2'
 #CA_CERT_FILENAME = '/home/leenr/Projects/appscale_run/local-cert.pem'
 
+GLOBAL_SECRET = '97677a572e5a450f91a06b6ad8f15116'
+UASERVER_HOST = '104.154.230.83'
+
 #AVAILABLE_APPS = ['blitztinyarmypanoramic']
 
 DEBUG = True
